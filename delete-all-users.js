39acb8baa8e68fa3ca637a7baef53dd36d5de13b
Juan Martin <juansm91@gmail.com>
2021-05-22 03:31:05 -0300

    corrected code for delete-all-users

--- delete-all-users.js
+++ delete-all-users.js
@@ -3,33 +3,31 @@ const fs = require("fs")
 const fetch = require("node-fetch")
 
 
-const deleteAllUsers = async () => {
-  await fetch(`${process.env.AUTH0_HOST}/api/v2/users`,
-    {
-      method: "GET",
-      headers: {
-        "Authorization": "Bearer " + process.env.AUTH0_TOKEN,
-        "Content-Type": "application/json"
-      }
-    }).then(res => res.json())
-    .then(data => {
-      fs.writeFileSync("auth0-users-backup.json", JSON.stringify(data))
-      console.log('Deleting all users from Auth0.')
-      data.forEach((u, i) => {
-        setTimeout(() => {
-          fetch(`${process.env.AUTH0_HOST}/api/v2/users/auth0|${u.identities[0].user_id}`,
-            {
-              method: "DELETE",
-              headers: {
-                "Authorization": "Bearer " + process.env.AUTH0_TOKEN,
-              }
-            })
-            .then(data => console.log('Deleted User', u.identities[0].user_id, "| Response: ", data.status))
-            .catch(err => console.error("Error deleting user.", err))
-        }, i * 1000)
-      })
+fetch(`${process.env.AUTH0_HOST}/api/v2/users`,
+  {
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer " + process.env.AUTH0_TOKEN,
+      "Content-Type": "application/json"
+    }
+  }).then(res => res.json())
+  .then(data => {
+    fs.writeFileSync("auth0-users-backup.json", JSON.stringify(data))
+    console.log('Deleting all users from Auth0.')
+    data.forEach((u, i) => {
+      setTimeout(() => {
+        fetch(`${process.env.AUTH0_HOST}/api/v2/users/auth0|${u.identities[0].user_id}`,
+          {
+            method: "DELETE",
+            headers: {
+              "Authorization": "Bearer " + process.env.AUTH0_TOKEN,
+            }
+          })
+          .then(data => console.log('Deleted User', u.identities[0].user_id, "| Response: ", data.status))
+          .catch(err => console.error("Error deleting user.", err))
+      }, i * 1000)
     })
-    .catch(err => console.error("Error getting users.", err))
-}
+  })
+  .catch(err => console.error("Error getting users.", err))
 
-deleteAllUsers()
+
